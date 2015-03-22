e9c7d5e82a3e7515c68b6e4621e6a1b29e7ee4dc
Eliot Whalan <ewhalan@hotmail.com>
2015-03-22 15:54:50 +1000

    Added more addons and fixed addon installation part

--- install.sh
+++ install.sh
@@ -6,7 +6,7 @@ echo "Please enter your firefox profile name"
 read PROFILE
 
 FILES=".zshrc .Xresources .vimrc .weechat .i3 .tmux.conf .bin .xinitrc .ncmpcpp .conkyrc .mpd .vimperatorrc"
-ADDONS="https://addons.mozilla.org/firefox/downloads/latest/722/addon-722-latest.xpi https://www.eff.org/files/https-everywhere-latest.xpi https://addons.mozilla.org/firefox/downloads/latest/334572/addon-334572-latest.xpi https://addons.mozilla.org/firefox/downloads/latest/953/addon-953-latest.xpi https://addons.mozilla.org/firefox/downloads/file/161685/greasemonkey-0.9.22-fx.xpi https://addons.mozilla.org/firefox/downloads/latest/2108/addon-2108-latest.xpi https://addons.mozilla.org/firefox/downloads/file/298890/vimperator-3.9-fx.xpi https://github.com/RequestPolicyContinued/requestpolicy/releases/download/v1.0.beta8.2/requestpolicy-1-0-beta8-2.xpi https://addons.mozilla.org/firefox/downloads/file/120824/open_with-5.0.3-sm+fx.xpi https://addons.mozilla.org/firefox/downloads/file/299918/block-0.8.8.2-fx.xpi"
+ADDONS="https://addons.mozilla.org/firefox/downloads/latest/722/addon-722-latest.xpi https://www.eff.org/files/https-everywhere-latest.xpi https://addons.mozilla.org/firefox/downloads/latest/334572/addon-334572-latest.xpi https://addons.mozilla.org/firefox/downloads/latest/953/addon-953-latest.xpi https://addons.mozilla.org/firefox/downloads/file/161685/greasemonkey-0.9.22-fx.xpi https://addons.mozilla.org/firefox/downloads/latest/2108/addon-2108-latest.xpi https://addons.mozilla.org/firefox/downloads/file/298890/vimperator-3.9-fx.xpi https://github.com/RequestPolicyContinued/requestpolicy/releases/download/v1.0.beta8.2/requestpolicy-1-0-beta8-2.xpi https://addons.mozilla.org/firefox/downloads/file/120824/open_with-5.0.3-sm+fx.xpi https://addons.mozilla.org/firefox/downloads/file/299918/block-0.8.8.2-fx.xpi https://code.google.com/p/https-finder/downloads/detail?name=httpsfinder_0.91b.xpi&can=2&q= https://github.com/YePpHa/YouTubeCenter/raw/master/dist/YouTubeCenter.user.js https://ccd0.github.io/4chan-x/builds/4chan-X.user.js"
 
 cwd=$(pwd)
 
@@ -28,12 +28,10 @@ echo "Setting up firefox"
 
 for a in $ADDONS
 do
-   "wget $a"
+    firefox $a
 done
 
-firefox *.xpi
-rm *.xpi
-ln -fs $(pwd)/twily.vimp /home/$NAME/colors/twily.vimp
+ln -fs $(pwd)/twily.vimp /home/$NAME/.vimperator/colors/twily.vimp
 echo "Please visit twily.info for more Firefox rice and run vim to install plugins"
 
 
